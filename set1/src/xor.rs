6ace3fd5a6a5b65ed169465a4c8ba7868148d8d1
Alex Xiong <alex.xiong.tech@gmail.com>
2019-12-16 10:09:22 +0800

    set1 chal5

--- set1/src/xor.rs
+++ set1/src/xor.rs
@@ -1,3 +1,4 @@
+use crate::base64;
 use std::iter::FromIterator;
 
 pub fn fixed_xor(hex1: &str, hex2: &str) -> Result<String, &'static str> {
@@ -15,6 +16,32 @@ pub fn fixed_xor(hex1: &str, hex2: &str) -> Result<String, &'static str> {
     Ok(String::from_iter(xor))
 }
 
+// key and msg are not hex string slice, but the actual content
+pub fn repeating_xor(msg: &str, key: &str) -> String {
+    let msg_hex = base64::bytes_to_hex(&msg.as_bytes().to_vec());
+    let key_hex = base64::bytes_to_hex(&key.as_bytes().to_vec());
+    let mut xor: String = String::new();
+
+    for i in 0..(msg_hex.len() / key_hex.len()) {
+        xor.push_str(
+            &fixed_xor(
+                &msg_hex[i * key_hex.len()..(i + 1) * key_hex.len()],
+                &key_hex[..],
+            )
+            .unwrap(),
+        );
+    }
+    // dealing with remainder
+    let remainder_len = msg_hex.len() % key_hex.len();
+    xor.push_str(
+        &fixed_xor(
+            &msg_hex[msg_hex.len() - remainder_len..],
+            &key_hex[0..remainder_len],
+        )
+        .unwrap(),
+    );
+    xor
+}
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -31,6 +58,14 @@ mod tests {
         assert_eq!(
             fixed_xor("12", "3"),
             Err("two hex string should have the same length"),
+        );
+    }
+
+    #[test]
+    fn test_repeating_xor() {
+        assert_eq!(
+            repeating_xor("Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal", "ICE"),
+            "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f",
         );
     }
 }

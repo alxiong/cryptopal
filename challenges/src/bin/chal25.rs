c26c0f4d0c8aa75e39ad11f577ca6697970e97bf
AlexXiong97 <alex.xiong.tech@gmail.com>
2020-01-18 17:31:57 +0800

    finish chal25

---
+++ challenges/src/bin/chal25.rs
@@ -0,0 +1,34 @@
+use challenges::random_bytes_array;
+use cipher::{ctr::AES_128_CTR, Cipher};
+use std::fs;
+use xor;
+
+fn main() {
+    println!("🔓 Challenge 25");
+    let ctr_cipher = AES_128_CTR::new_with_nonce(0);
+    let pt = fs::read_to_string("challenges/data/chal25.txt")
+        .unwrap()
+        .as_bytes()
+        .to_vec();
+    let mut key = vec![0 as u8; 16];
+    random_bytes_array(&mut key);
+    let ct = ctr_cipher.encrypt(&key, &pt);
+
+    let decrypted = break_ctr(&ct, &key);
+    assert_eq!(decrypted, pt);
+    println!("Successfully decrypted");
+}
+
+fn break_ctr(ct: &[u8], key: &[u8]) -> Vec<u8> {
+    let craft_pt = vec![0 as u8; ct.len()];
+    let key_stream = xor::xor(&craft_pt, &edit(&ct, &key, 0, &craft_pt)).unwrap();
+    xor::xor(&key_stream, &ct).unwrap()
+}
+
+// chosen plaintext with fixed nounce
+fn edit(ct: &[u8], key: &[u8], offset: usize, newtext: &[u8]) -> Vec<u8> {
+    let ctr_cipher = AES_128_CTR::new_with_nonce(0);
+    let mut new_pt = ctr_cipher.decrypt(&key, &ct);
+    new_pt.splice(offset.., newtext.iter().cloned());
+    ctr_cipher.encrypt(&key, &new_pt)
+}

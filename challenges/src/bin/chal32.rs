ddca1edbac3bfc18f48e6b163c94ab44fd9b6357
AlexXiong97 <alex.xiong.tech@gmail.com>
2020-02-01 14:45:19 +0800

    finish chal32

---
+++ challenges/src/bin/chal32.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!("🔓 Challenge 32");
+    println!("1. Change L14 in 'challenges/src/chal31.rs' from 50 to 5");
+    println!("2. Change L67 in 'challenges/src/bin/chal31.rs' from 50 to 5");
+    println!("3. Then run 'cargo run --bin chal31'");
+}

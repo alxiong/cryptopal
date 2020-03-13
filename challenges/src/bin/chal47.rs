ba59fd5aca507cdc814894e6f6796e7e8fa69cc4
Alex Xiong <alex.xiong.tech@gmail.com>
2020-03-13 18:34:06 +0800

    improve bleichenbacher impl

--- challenges/src/bin/chal47.rs
+++ challenges/src/bin/chal47.rs
@@ -4,12 +4,12 @@ use challenges::chal48::{rsa_padding_oracle_attack, Oracle};
 fn main() {
     println!("🔓 Challenge 47");
     let rsa_keys = RsaKeyPair::new_256_rsa();
-    let oracle = Oracle::new(&rsa_keys);
+    let mut oracle = Oracle::new(&rsa_keys);
 
     let msg = b"kick it, CC".to_vec();
     let m = rsa_keys.pubKey.pkcs_pad(&msg);
     let ct = &rsa_keys.pubKey.encrypt(&m);
 
-    let decrypted = rsa_padding_oracle_attack(&rsa_keys.pubKey, &ct, &oracle);
+    let decrypted = rsa_padding_oracle_attack(&rsa_keys.pubKey, &ct, &mut oracle);
     assert_eq!(decrypted, m);
 }
--- challenges/src/chal48.rs
+++ challenges/src/chal48.rs
@@ -2,105 +2,115 @@ use super::chal39::{RsaKeyPair, RsaPubKey};
 use super::mod_inv;
 use num::{bigint::RandBigInt, pow::Pow, BigUint, FromPrimitive, One, Zero};
 use std::cmp;
+use std::fmt;
+use std::thread;
+use std::time::Duration;
 
 #[allow(clippy::many_single_char_names, non_snake_case)]
 /// Implementation of BB'98 CCA padding oracle attack, returns the decrypted message
-pub fn rsa_padding_oracle_attack(pk: &RsaPubKey, ct: &BigUint, oracle: &Oracle) -> BigUint {
+pub fn rsa_padding_oracle_attack(pk: &RsaPubKey, ct: &BigUint, oracle: &mut Oracle) -> BigUint {
+    // For UX only
+    let mut minutes = 1;
+    let _ = thread::spawn(move || loop {
+        thread::sleep(Duration::from_secs(60));
+        println!("⌛ {} minutes has passed!", minutes);
+        minutes += 1;
+    });
+
     println!("Bleichenbacher padding oracle attack ... (take many many minutes)");
-    let n = pk.n.clone();
+    let n = &pk.n.clone();
     let mut rng = rand::thread_rng();
     let mut M: Vec<Range> = vec![];
     let mut s_last = BigUint::zero();
     let mut s_new = BigUint::zero();
     // B = 2 ^ (n - 16)
-    let B = BigUint::from_u32(2).unwrap().pow(&n.bits() - 16);
-    let B_double = &B * BigUint::from_u32(2).unwrap();
-    let B_triple = &B * BigUint::from_u32(2).unwrap();
-    let B_triple_minus_one = &B_triple - BigUint::one();
+    let two = &BigUint::from_u32(2).unwrap();
+    let three = &BigUint::from_u32(3).unwrap();
+    let B = &(two.pow(n.bits() - 16));
+    let B_double = &(B * two);
+    let B_triple = &(B * three);
+    let B_triple_minus_one = &(B_triple - BigUint::one());
 
     // Step 1: Blinding
-    println!("Starting step 1: blinding ...");
-    let mut s_0 = BigUint::zero();
-    while s_0 == BigUint::zero() || !oracle.oracle_query(&(ct * pk.encrypt(&s_0))) {
-        s_0 = rng.gen_biguint_below(&pk.n);
-    }
+    println!("\nStarting step 1: blinding ...");
+    let s_0 = BigUint::one();
     M.push(Range::new(&B_double, &B_triple_minus_one));
 
     let mut i = 1;
     // =================
+    println!("\nStarting step 2~4: searching and narrowing ...");
     loop {
         // Step 2: Adaptive chosen s value search
         if i == 1 {
             // step 2.a starting the search
-            for s in Range::new(&(&n / &B_triple), &n) {
-                // the smallest s >= n/3B that's PKCS conforming
-                if oracle.oracle_query(&(ct * pk.encrypt(&s))) {
-                    s_new = s;
-                }
+            println!("\nExecuting 2.a");
+            while s_new == BigUint::zero() || !oracle.oracle_query(&(ct * pk.encrypt(&s_new))) {
+                s_new = rng.gen_biguint_range(&(n / B_triple), n);
+            }
+        } else if M.len() > 1 {
+            // step 2.b
+            println!("\nExecuting 2.b");
+            while s_new == s_last || !oracle.oracle_query(&(ct * pk.encrypt(&s_new))) {
+                s_new += BigUint::one();
             }
         } else {
-            if M.len() > 1 {
-                // step 2.b
-                for s in Range::new(&s_last, &n) {
+            // step 2.c
+            println!("\nExecuting 2.c");
+            assert_eq!(M.len(), 1);
+            let a = &M[0].start;
+            let b = &M[0].stop;
+
+            // r_i >= 2 * (b * s_i-1 - B) / n
+            let r_min = two * (b * &s_last - B) / n;
+            'outer: for r in Range::new(&r_min, n) {
+                let s_min = (B_double + &r * n) / b;
+                let s_max = (B_triple + &r * n) / a;
+
+                for s in Range::new(&s_min, &s_max) {
                     if oracle.oracle_query(&(ct * pk.encrypt(&s))) {
                         s_new = s;
+                        break 'outer;
                     }
-                }
-            } else {
-                // step 2.c
-                assert_eq!(M.len(), 1);
-                let a = &M[0].start;
-                let b = &M[0].stop;
-                // ri >= 2 * (b * s_i-1 - B) / n
-                let mut r_min = &BigUint::from_u32(2).unwrap() * (b * &s_last - &B) / &n;
-                'outer: loop {
-                    let s_min = (&B_double + &r_min * &n) / b;
-                    let s_max = (&B_triple + &r_min * &n) / a;
-                    for s in Range::new(&s_min, &s_max) {
-                        if oracle.oracle_query(&(ct * pk.encrypt(&s))) {
-                            s_new = s;
-                            break 'outer;
-                        }
-                    }
-                    r_min += BigUint::one();
                 }
             }
         }
+
+        assert!(oracle.oracle_query(&(ct * pk.encrypt(&s_new))));
         println!("New s_i: {}", &s_new);
         // =================
 
         // Step 3: Narrowing the solution range
-        M = M
-            .into_iter()
-            .flat_map(|interval| {
-                let a = &interval.start;
-                let b = &interval.stop;
-                let r_min = ceil(&(a * &s_new - &B_triple_minus_one), &n);
-                let r_max = (b * &s_new - &B_double) / &n;
+        println!("\nExecuting narrowing");
+        let mut M_new: Vec<Range> = vec![];
+        M.into_iter().for_each(|interval| {
+            let a = &interval.start;
+            let b = &interval.stop;
+            let r_min = div_ceil(&(a * &s_new - B_triple_minus_one), n);
+            let r_max = (b * &s_new - B_double) / n;
 
-                let mut range_candidates = vec![];
-                for r in Range::new(&r_min, &r_max) {
-                    let range_candidate = Range {
-                        start: (&B_double + &r * &n) / &s_new,
-                        stop: (&B_triple_minus_one + &r * &n) / &s_new,
-                    };
-                    if let Some(intersect) = range_candidate.intersect(&interval) {
-                        range_candidates.push(intersect);
-                    }
+            for r in Range::new(&r_min, &r_max) {
+                let range_candidate = Range {
+                    start: div_ceil(&(B_double + &r * n), &s_new),
+                    stop: (B_triple_minus_one + &r * n) / &s_new,
+                };
+
+                if let Some(intersect) = range_candidate.intersect(&interval) {
+                    M_new.push(intersect);
                 }
-                if range_candidates.len() == 0 {
-                    vec![interval]
-                } else {
-                    range_candidates
-                }
-            })
-            .collect();
+            }
+
+            if M_new.is_empty() {
+                M_new.push(interval);
+            }
+        });
+        M = M_new;
         println!("M: {:?}", M);
         // =================
 
         // Step 4: Terminate or Repeat (back to step 2)
         if M.len() == 1 && M[0].start == M[0].stop {
-            return (&M[0].start * mod_inv(&s_0, &n).unwrap()) % n;
+            println!("\nTotal queries: {}", oracle.query_times.to_str_radix(10));
+            return (&M[0].start * mod_inv(&s_0, n).unwrap()) % n;
         } else {
             s_last = s_new.clone();
             i += 1;
@@ -112,21 +122,27 @@ pub fn rsa_padding_oracle_attack(pk: &RsaPubKey, ct: &BigUint, oracle: &Oracle) 
 /// Bleichenbacher oracle
 pub struct Oracle {
     key_pair: RsaKeyPair,
+    pub query_times: BigUint,
 }
 
 impl Oracle {
     pub fn new(key_pair: &RsaKeyPair) -> Oracle {
         Oracle {
             key_pair: key_pair.clone(),
+            query_times: BigUint::zero(),
         }
     }
 
-    pub fn oracle_query(&self, ct: &BigUint) -> bool {
-        self.key_pair.priKey.bb_oracle(&ct)
+    pub fn oracle_query(&mut self, ct: &BigUint) -> bool {
+        self.query_times += BigUint::one();
+        // could use the following to keep track of query times
+        // if &self.query_times % &BigUint::parse_bytes(b"1024", 10).unwrap() == BigUint::zero() {
+        //     println!("Another 2^10 queries");
+        // }
+        self.key_pair.priKey.bb_oracle(&(ct % &self.key_pair.pubKey.n))
     }
 }
 
-#[derive(Debug)]
 // inclusive range [start, stop]
 struct Range {
     pub start: BigUint,
@@ -135,6 +151,7 @@ struct Range {
 
 impl Range {
     pub fn new(start: &BigUint, stop: &BigUint) -> Self {
+        assert!(start <= stop);
         Range {
             start: start.clone(),
             stop: stop.clone(),
@@ -153,6 +170,15 @@ impl Range {
     }
 }
 
+impl fmt::Debug for Range {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_list()
+            .entry(&self.start.to_str_radix(16))
+            .entry(&self.stop.to_str_radix(16))
+            .finish()
+    }
+}
+
 impl Iterator for Range {
     type Item = BigUint;
     fn next(&mut self) -> Option<Self::Item> {
@@ -167,7 +193,7 @@ impl Iterator for Range {
 }
 
 // returns ceiling(num/dem)
-fn ceil(num: &BigUint, dem: &BigUint) -> BigUint {
+fn div_ceil(num: &BigUint, dem: &BigUint) -> BigUint {
     let rem = num % dem;
     if rem > BigUint::zero() {
         num / dem + BigUint::one()
